22f1aad54808f6586077cd28fde1504ecf6fd937
Dan Adler <2274667+dadler64@users.noreply.github.com>
2019-07-16 19:20:53 -0400

    Updated: Version now reflects that this project is in Alpha stages and there is now a command line option to launch the old Swing/AWT GUI

--- src/main/kotlin/com/adlerd/compsim/CompSim.kt
+++ src/main/kotlin/com/adlerd/compsim/CompSim.kt
@@ -3,15 +3,14 @@ package com.adlerd.compsim
 import com.adlerd.compsim.core.CommandLine
 import com.adlerd.compsim.core.Machine
 import com.adlerd.compsim.core.TempRun
+import com.adlerd.compsim.gui.fx.CompSimGUI
 import com.adlerd.compsim.gui.fxml.CompSimTestGUI
 import com.adlerd.compsim.gui.swing.LC3GUI
-import com.adlerd.compsim.gui.tornado.CompSimTFX
 import com.adlerd.compsim.util.ErrorLog
 import com.adlerd.compsim.util.Logger.errorln
 import com.adlerd.compsim.util.Logger.infoln
 import com.adlerd.compsim.util.exceptions.GenericException
 import javafx.application.Application
-import tornadofx.launch
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
@@ -19,7 +18,7 @@ import javax.swing.SwingUtilities
 
 object CompSim {
     const val TITLE = "CompSim"
-    const val VERSION = "1.0.0"
+    const val VERSION = "1.0.0 - Alpha"
     var isGraphical = true
     var isSwing = false
     var isFXML = false
@@ -35,29 +34,32 @@ object CompSim {
             else null
         }
 
-    fun getVersion(): String {
-        return "$TITLE Version $VERSION"
-    }
+    var version: String = ""
+        get() = "$TITLE Version $VERSION"
+        private set
 
     private fun printUsage() {
-        println("\nUsage: java CompSim [-lc3] [-p37x] [-pipeline] [-t] [-s <script>]")
+        println("\nUsage: java CompSim [-lc3] [-p37x] [-pipeline] [-t | -fxml | -swing] [-s <script>]")
         println("  -lc3 : simulate the LC-3 ISA")
+        println("  -fxml : launch experimental FXML GUI")
         println("  -p37x : simulate the P37X ISA")
         println("  -pipeline : simulate a 5-stage fully-bypassed pipeline")
         println("  -t : start in command-line mode")
         println("  -s script : run 'script' from a script file")
+        println("  -swing: launch the older Swing/AWT GUI")
     }
 
     @JvmStatic
     fun main(args: Array<String>) {
         var str: String? = null
-        infoln("${getVersion()}\n")
+        infoln(version)
 
         var i = 0
         while (i < args.size) {
             when {
                 args[i].equals("-t", ignoreCase = true) -> isGraphical = false
                 args[i].equals("-fxml", ignoreCase = true) -> isFXML = true
+                args[i].equals("-swing", ignoreCase = true) -> isSwing = true
                 args[i].equals("-s", ignoreCase = true) -> {
                     ++i
                     if (i >= args.size) {
@@ -102,19 +104,19 @@ object CompSim {
             if (isGraphical) {
                 when {
                     isSwing -> {
-                        infoln("Loading Swing/AWT graphical interface\n")
+                        infoln("Loading Swing/AWT graphical interface")
                         LC3GUI.initLookAndFeel()
                         val gui = LC3GUI(machine, commandLine)
                         machine.LC3GUI = gui
                         SwingUtilities.invokeLater(TempRun(gui))
                     }
                     isFXML -> {
-                        infoln("Loading experimental JavaFX FXML graphical interface\n")
+                        infoln("Loading experimental JavaFX FXML graphical interface")
                         Application.launch(CompSimTestGUI::class.java)
                         infoln("GUI closed...")
                     }
                     else -> {
-                        infoln("Loading JavaFX graphical interface\n")
+                        infoln("Loading JavaFX graphical interface")
                         Application.launch(CompSimGUI::class.java)
                         infoln("GUI closed...")
                     }
